af480a2df0bf5244d033251e7f48f01c2062145c
AlexDuart735 <alexduart735@gmail.com>
2025-11-27 20:20:07 -0300

    persistencia pronta

--- PersistenciaDeUsuario.cpp
+++ PersistenciaDeUsuario.cpp
@@ -8,8 +8,80 @@ list<Usuario*>* PersistenciaDeUsuario::carregar(string arquivoProfessor, string 
     ifstream input;
     input.open(arquivoProfessor);
 
+    if(input.fail()) {
+        throw new logic_error("Arquivo nao encontrado");
+    }
+
     int nusp;
-    input >> nusp;
-    string nome;
-    input >> nome;
+    string nome, departamento;
+
+    while(input) {    
+        input >> nusp;
+        input >> nome;
+        input >> departamento;
+        usuarios->push_back(new Professor(nusp, nome, departamento));
+    }
+
+    if(!input.eof()) {
+        throw new logic_error("Erro de leitura");
+    }
+
+    input.close();
+
+    input.open(arquivoAluno);
+
+    if(input.fail()) {
+        throw new logic_error("Aequivo nao encontrado");
+    }
+
+    while(input) {
+        input >> nusp;
+        input >> nome;
+        usuarios->push_back(new Aluno(nusp, nome));
+    }
+
+    if(!input.eof()) {
+        throw new logic_error("Erro de leitura");
+    }
+
+    input.close();
+
+    return usuarios;
+
+}
+
+void PersistenciaDeUsuario::salvar(string arquivoProfessor, string arquivoAluno, list<Usuario*>* usuarios) {
+    ofstream output;
+    output.open(arquivoProfessor);
+
+    if(output.fail()) {
+        throw new logic_error("Erro ao escrever");
+    } else { 
+        list<Usuario*>::iterator i = usuarios->begin();
+        while(i != usuarios->end()) {
+            Professor* prof = dynamic_cast<Professor*>((*i));
+            if(prof != nullptr) {
+                output << prof->getNusp() << " " << prof->getNome() << " " << prof->getUnidade() << endl;
+            }
+        }
+    }    
+
+    output.close();
+
+    output.open(arquivoAluno);
+
+    if(output.fail()) {
+        throw new logic_error("Erro ao escrever");
+    } else {
+        list<Usuario*>::iterator i = usuarios->begin();
+        while(i != usuarios->end()) {
+            Aluno* aluno = dynamic_cast<Aluno*>((*i));
+            if(aluno != nullptr) {
+                output << aluno->getNusp() << " " << aluno->getNome() << endl;
+            }
+        }
+    }
+
+    output.close();
+
 }
--- PersistenciaDeUsuario.h
+++ PersistenciaDeUsuario.h
@@ -4,6 +4,8 @@
 #include <stdexcept>
 
 #include "Usuario.h"
+#include "Professor.h"
+#include "Aluno.h"
 
 #ifndef PERSISTENCIA_DE_USUARIO_H
 #define PERSISTENCIA_DE_USUARIO_H
@@ -11,12 +13,14 @@
 using namespace std;
 
 class PersistenciaDeUsuario {
-    public:
-        PersistenciaDeUsuario();
-        virtual ~PersistenciaDeUsuario();
+public:
+    PersistenciaDeUsuario();
+    virtual ~PersistenciaDeUsuario();
 
-        list<Usuario*>* carregar(string arquivoProfessor, string arquivoAluno);
-        void salvar(string arquivoProfessor, string arquivoAluno, list<Usuario*>* usuarios);
+    list<Usuario*>* carregar(string arquivoProfessor, string arquivoAluno);
+    void salvar(string arquivoProfessor, string arquivoAluno, list<Usuario*>* usuarios);
+private:
+    list<Usuario*>* usuarios;
 };
 
 #endif
